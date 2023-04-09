557025ecbfba417807993d4a077f117eeeef2176
simonteclai <nomistec.st@gmail.com>
2023-04-09 13:14:04 -0700

    3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" Fabfile based on file 2-do_deploy_web_static.py that
+creates and distributes an archive to the web servers """
+
+from datetime import datetime
+from fabric.api import *
+import os
+
+env.hosts = ["54.237.79.150", "18.206.198.73"]
+env.user = "ubuntu"
+
+
+def do_pack():
+    """
+        return the archive path if archive generate properly.
+    """
+
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_f_path
+    else:
+        return None
+
+
+def do_deploy(archive_path):
+    """  Distributes archive."""
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+
+        print("New version deployed!")
+        return True
+
+    return False
+
+
+def deploy():
+    """
+    Deploy function do_pack & do_deploy.
+    """
+    path = do_pack()
+    if path:
+        do_deploy(path)
+    return False
